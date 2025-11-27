2a6f38d7cbd18bbbe293d6b1c1f53e26839ca43b
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-11-26 20:53:39 -0400

    feat: add area model

---
+++ src/models/Area.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const AreaSchema = new mongoose.Schema(
+    {
+        area_name: { 
+            type: String, 
+            required: true 
+        },
+        area_description: { 
+            type: String, 
+            required: false 
+        },
+        acronym: { 
+            type: String, 
+            maxlength: 5,
+            required: false 
+        },
+        id_subject: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Subject', 
+            required: true,
+            unique: true
+        }
+    }, 
+    { 
+        timestamps: true 
+    }
+);
+
+AreaSchema.index({ id_materia: 1 }, { unique: true });
+
+const Area = mongoose.model('Area', AreaSchema);
+
+export default Area;
