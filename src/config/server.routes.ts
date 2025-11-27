13cc362dd05461fbdb68704b283a1439ec4bef4b
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-11-26 21:37:09 -0400

    fix: add question routes

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -4,6 +4,7 @@ import QuestionAgeClassificationRoutes from '../routes/questionAgeClassification
 import QuestionTypeRoutes from '../routes/questionType.routes';
 import AreaRoutes from '../routes/area.routes';
 import SubjectRoutes from '../routes/subject.routes';
+import QuestionRoutes from '../routes/question.routes';
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.use('/question_age_classification', QuestionAgeClassificationRoutes);
 router.use('/area', AreaRoutes);
 router.use('/question_type', QuestionTypeRoutes);
 router.use('/subject', SubjectRoutes);
+router.use('/question', QuestionRoutes);
 
 router.use((req, res) => {
   console.log('Not found:', req.method, req.originalUrl);
