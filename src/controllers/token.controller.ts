b70786043cd19c9cc71927cd74a5a1b0c1dfd4cb
gg1700 <gabrielcamacho1700@gmail.com>
2025-12-04 04:47:14 -0400

    feat: update token controller to include request and response parameters

--- src/controllers/token.controller.ts
+++ src/controllers/token.controller.ts
@@ -1,11 +1,25 @@
-export async function sendToken() {
+import { Request, Response } from "express";
 
+export async function sendToken(req: Request, res: Response) {
+    const token = null;
+    res.send({ token })
 }
 
-export async function publicToken() {
-
+export async function publicToken(req: Request, res: Response) {
+    res.status(200).json({
+        message: 'Publico'
+    });
 }
 
-export async function privateToken() {
-
+export async function privateToken(req: Request, res: Response) {
+    try {
+        res.status(200).json({
+            message: 'Privado'
+        })
+    }catch (err) {
+        res.status(400).json({
+            message: 'Error al acceder',
+            error: (err as Error).message
+        })
+    }
 }
