86afba18e832dd18084acc3866e1a623769373cb
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-12-11 01:23:39 -0400

    feat: added https validation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+
 import Server from './config/server.config';
-
 import { SERVER_PORT } from './config/env.config';
-
 import { connectDB } from './config/db/mongoClient';
 
 async function startServer() {
@@ -21,6 +23,16 @@ async function startServer() {
     Server.listen(SERVER_PORT, () => {
       console.info(`Server running on http://localhost:${SERVER_PORT}`);
     });
+
+    const privateKey = fs.readFileSync('localhost-key.pem', 'utf8');
+    const certificate = fs.readFileSync('localhost.pem', 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
+
+    const httpsServer = https.createServer(credentials, Server);
+    httpsServer.listen(8443, () => {
+      console.log(`Servidor HTTPS escuchando en https://localhost:${8443}`);
+    });
+
   } catch (error) {
     console.error('Error starting server', error);
   }
